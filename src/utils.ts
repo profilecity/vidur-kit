4096ebcbad23e007d733713375afaf78fee5bec8
amandesai01 <amandesai01@gmail.com>
2024-07-19 15:43:29 +0530

    fix: message composition

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,3 @@
 const PACKAGE_STUB = "@profilecity/vidur-kit"
 
-export const composeMessage = (message: string) => `${PACKAGE_STUB} ${message}`;
+export const composeMessage = (message: string) => `[${PACKAGE_STUB}] ${message}`;
